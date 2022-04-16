2e2341b82d2f1469e58fc190d33da9a9de1092f8
LHD <sunupdong@gmail.com>
2022-04-17 00:32:27 +0800

    :memo: readme

--- babel.config.js
+++ babel.config.js
@@ -1,10 +1,10 @@
 module.exports = {
   presets: [
     // 配置规则
-    "@babel/preset-env",
+    '@babel/preset-env',
     // 支持vue中的jsx语法
-    "@vue/babel-preset-jsx"
+    '@vue/babel-preset-jsx',
   ],
   // 转换async/await所需插件
-  plugins: ["@babel/plugin-transform-runtime"]
+  plugins: ['@babel/plugin-transform-runtime'],
 }
